94ae3936129593c2e9e4bf6c42413350503ff9eb
jesus <jbarz20@gmail.com>
2023-01-30 13:00:06 -0500

    ADD: install react table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
 import { getUsers } from "./services/users-sessions";
 
-
+const Row = styled.div`
+  display:flex;
+  gap: 20px;
+  border: 1px solid black;
+`
 
 function App() {
-
-  getUsers().then(response => console.log(response.results)).catch(console.log)
-
+  const [users, setUsers] = useState(null)
+  useEffect(() => {
+    getUsers()
+      .then(response => {
+        setUsers(response.results)
+        console.log(response.results)
+      })
+      .catch(console.log)
+    
+  }, [])
+  
   return (
-    <h1>Hola</h1>
+    users ? users.map(user => {
+      return  <Row>
+                <h1>{user.name.first}</h1>
+                <h1>{user.name.last}</h1>
+                <h1>{user.dob.age}</h1>
+                <h1>{ user.gender == "female" ? "F" : "M" }</h1>
+                <h1>{user.email}</h1>
+                <h1>{user.nat}</h1>
+                <img src={user.picture.thumbnail} />
+              </Row>
+    }) : null
   );
 }
 
